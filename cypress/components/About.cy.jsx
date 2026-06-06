9652c67e026604d75f04139328d4416559669e16
Helenio Padrón Álvarez <helenio@gextiona.com>
2026-06-06 11:50:26 +0000

    test: actualizar component test About.cy.jsx para el nuevo About

--- cypress/components/About.cy.jsx
+++ cypress/components/About.cy.jsx
@@ -1,13 +1,37 @@
 /// <reference types="Cypress" />
-// import React from 'react'
-import About from '../../src/components/About'
+import About from '../../src/components/About';
+
+const expectedMembers = [
+  'Álvaro Cebrián Urueña',
+  'Xabier Arroitajauregui Elguea',
+  'Fernando Javier Rodríguez García Rendueles',
+  'Naiara Azpeitia Azcue',
+  'Helenio Padrón Álvarez',
+];
 
 describe('<About />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<About />).then(() => {
-      cy.get('h1').should('contain', 'Acerca de nosotros')
-      cy.get('#info').should('contain', 'Somos el equipo nº').contains(/[1-30]/)
-    })  
-  })
-})
+  beforeEach(() => {
+    cy.mount(<About />);
+  });
+
+  it('muestra el título "Quienes somos"', () => {
+    cy.get('h1').should('contain', 'Quienes somos');
+  });
+
+  it('muestra el número de equipo N04', () => {
+    cy.get('.team-number').should('contain', 'N04');
+  });
+
+  it('muestra los 5 nombres del equipo', () => {
+    expectedMembers.forEach((name) => {
+      cy.contains('.member-name', name).should('be.visible');
+    });
+  });
+
+  it('muestra una descripción no vacía para cada miembro', () => {
+    cy.get('.member-description').should('have.length', 5);
+    cy.get('.member-description').each(($el) => {
+      cy.wrap($el).invoke('text').should('not.be.empty');
+    });
+  });
+});
